eddc13655b0d146bf58c9f8aad645c01d2ffc1f9
diisk127 <kanyawee.si@kkumail.com>
2025-08-22 20:47:26 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,55 +1,51 @@
 # ฟังก์ชันการบวก
-def add(a,b):
-  return round(a + b, 2)
+def add(a, b):
+    pass  
 
 # ฟังก์ชันการลบ
-def subtract(a,b):
-  return round(a - b, 2)
+def subtract(a, b):
+    pass
 
 # ฟังก์ชันการคูณ
-def multiply(a,b):
-  return round(a * b,2)
+def multiply(a, b):
+    pass
 
 # ฟังก์ชันการหาร
-def divide(a,b):
-  # ถ้าตัวหาร (b) = 0
-  if b == 0:
-    return "Cannot divide by zero"
-  return round(a / b, 2)
+def divide(a, b):
+    pass
 
 # main function
 def calculator():
-  # ใช้ while True
-  while True:
-    print("-------- Calculator Menu --------")
-    print("1. Add Function")
-    print("2. Subtract Function")
-    print("3. Multiply Function")
-    print("4. Divide Function")
-    print("5. Exit")
-    choice = input("Enter your choice (1-5): ")
-    if choice not in ["1", "2", "3", "4", "5"]:
-      print("Please try again.")
-      continue
+    while True:
+        print("-------- Calculator Menu --------")
+        print("1. Add Function")
+        print("2. Subtract Function")
+        print("3. Multiply Function")
+        print("4. Divide Function")
+        print("5. Exit")
 
-    # ตรวจสอบ Exit
-    if choice == "5":
-      print("End of the program...")
-      break
+        choice = input("Enter your choice (1-5): ")
 
-    a = float(input("Enter the first number: "))
-    b = float(input("Enter the second number: "))
+        if choice == "5":
+            print("End of the program...")
+            break
 
-    if choice == "1":
-      print("Result: ", add(a, b))
-    elif choice == "2":
-      print("Result: ", subtract(a, b))
-    elif choice == "3":
-      print("Result: ", multiply(a, b))
-    elif choice == "4":
-      print("Result: ", divide(a, b))
-    else:
-      print("Invalid choice.")
+        # รับค่าตัวเลขจากผู้ใช้
+        a = float(input("Enter the first number: "))
+        b = float(input("Enter the second number: "))
 
+        # เรียกใช้ฟังก์ชันตามตัวเลือก
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        else:
+            print("Invalid choice.")
 
+# เรียกใช้งาน main function
 calculator()
+
